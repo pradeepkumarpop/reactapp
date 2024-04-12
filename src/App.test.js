065f259fe92519bbe4996481fcf7652773f96992
Ankush <ankush.kaura@mastersoftwaresolutions.com>
2024-04-12 15:54:50 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import App from './App';
 
-const thisProject = {
-  hasUnitTests: true,
-  hasE2ETests: true,
-  hasIssues: false
-};
-
-describe('my beverage', () => {
-  test('is delicious', () => {
-    expect(thisProject.hasUnitTests).toBeTruthy();
-  });
-
-  test('is not sour', () => {
-      expect(thisProject.hasE2ETests).toBeTruthy();
-  });
-
-  test('is not sour', () => {
-      expect(thisProject.hasIssues).toBeFalsy();
-  });
-});
-
-test('renders without crashing', () => {
-  const div = document.createElement('div');
-  render(<App />, div);
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
 });
